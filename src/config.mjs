76b13d8c36d409f211d3df2800fe45c68c7943be
Thomas YU <tyu@cegid.com>
2025-02-04 14:43:36 +0100

    Initial commit

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://thomasyu:rT0nTauipPEOH5aW@cluster0.xodyn.mongodb.net/api'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://thomasyu:rT0nTauipPEOH5aW@cluster0.xodyn.mongodb.net/api'
+  }
+};
